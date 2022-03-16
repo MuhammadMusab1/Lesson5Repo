aa81f0bc53168de0ea5adc5b89db08f489b4f737
Lussier07 <brendan.lussier07@gmail.com>
2022-03-16 13:51:23 -0500

    Add Journals and Relationships

--- WebApplication1/WebApplication1/Data/WebApplication1Context.cs
+++ WebApplication1/WebApplication1/Data/WebApplication1Context.cs
@@ -15,6 +15,51 @@ namespace WebApplication1.Data
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<User>().HasKey(c => c.Id);
+            builder.Entity<Journal>().HasKey(j => j.JournalNumber);
+            builder.Entity<UserJournal>().HasKey(uj => uj.EditingId);
+
+
+            builder.Entity<User>().HasMany(u => u.OwnedJournals)
+                .WithOne(u => u.Owner)
+                .HasForeignKey(u => u.JournalNumber);
+
+            builder.Entity<Journal>().HasOne(j => j.Owner)
+                .WithMany(j => j.OwnedJournals)
+                .HasForeignKey(j => j.OwnerId);
+
+
+            builder.Entity<UserJournal>().HasOne<User>(uj => uj.User)
+                .WithMany(uj => uj.EditingJournals)
+                .HasForeignKey(uj => uj.UserId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.Entity<UserJournal>().HasOne<Journal>(uj => uj.Journal)
+                .WithMany(uj => uj.JournalEditors)
+                .HasForeignKey(uj => uj.JournalNumber)
+                .OnDelete(DeleteBehavior.NoAction);
+
+
+            builder.Entity<User>().HasMany<UserJournal>(u => u.EditingJournals)
+                .WithOne(j => j.User)
+                .HasForeignKey(u => u.UserId);
+
+            builder.Entity<Journal>().HasMany<UserJournal>(j => j.JournalEditors)
+                .WithOne(j => j.Journal)
+                .HasForeignKey(j => j.JournalNumber);
+
+            builder.Entity<User>().HasData(new User 
+            {
+                Id = 1,
+                FirstName = "Zach",
+                LastName = "Montreuil"
+            });
+        }
+
         public DbSet<User> User { get; set; }
+        public DbSet<Journal> Journal { get; set; }
+        public DbSet<UserJournal> UserJournal { get; set; }
     }
 }
---
+++ WebApplication1/WebApplication1/Migrations/20220316184319_AddRelationshipsAndTouchUps.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    public partial class AddRelationshipsAndTouchUps : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OwnedJournalId",
+                table: "User",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Journal",
+                columns: table => new
+                {
+                    JournalNumber = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OwnerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Journal", x => x.JournalNumber);
+                    table.ForeignKey(
+                        name: "FK_Journal_User_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserJournal",
+                columns: table => new
+                {
+                    EditingId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    JournalNumber = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserJournal", x => x.EditingId);
+                    table.ForeignKey(
+                        name: "FK_UserJournal_Journal_JournalNumber",
+                        column: x => x.JournalNumber,
+                        principalTable: "Journal",
+                        principalColumn: "JournalNumber");
+                    table.ForeignKey(
+                        name: "FK_UserJournal_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_OwnedJournalId",
+                table: "User",
+                column: "OwnedJournalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Journal_OwnerId",
+                table: "Journal",
+                column: "OwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserJournal_JournalNumber",
+                table: "UserJournal",
+                column: "JournalNumber");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserJournal_UserId",
+                table: "UserJournal",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_User_Journal_OwnedJournalId",
+                table: "User",
+                column: "OwnedJournalId",
+                principalTable: "Journal",
+                principalColumn: "JournalNumber");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_User_Journal_OwnedJournalId",
+                table: "User");
+
+            migrationBuilder.DropTable(
+                name: "UserJournal");
+
+            migrationBuilder.DropTable(
+                name: "Journal");
+
+            migrationBuilder.DropIndex(
+                name: "IX_User_OwnedJournalId",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "OwnedJournalId",
+                table: "User");
+        }
+    }
+}
